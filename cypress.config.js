5bb2e560a78594fa8475f708803a95306235610f
PrzeC <prze.chmiel@gmail.com>
2023-06-18 08:45:39 +0200

    Revert "projekt_gotowy"
    This reverts commit 8826ffb9a8c43bf51b62df0b7d58a06a21c40a8b.

--- cypress.config.js
+++ cypress.config.js
@@ -6,18 +6,15 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   fixturesFolder: "cypress/fixtures",
   reporter: "mochawesome",
-  projectId: "75fcww",
   reporterOptions: {
     reportDir: "cypress/reports",
     overwrite: false,
     html: true,
     json: false,
+    projectId: "5bozs3",
   },
   e2e: {
     setupNodeEvents(on, config) {
     },
   },
-  env: {
-    CYPRESS_RECORD_KEY: "24ab4336-358c-4198-964e-071bdb3b9dde" // Zmień na swój klucz Cypress Dashboard
-  }
 });
